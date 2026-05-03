2012cb3da2d7268ba7722079e2f1a726ec14f5a0
PRIYESH RUDANI <priyeshrudani23@gnu.ac.in>
2026-05-03 21:36:46 +0530

    Allow requests from ANY frontend

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
